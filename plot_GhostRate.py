eb8e361ede47b1b91dba0e2356bed00e0167bd5b
Dinyar Rabady <dinyar.rabady@cern.ch>
2015-02-26 19:50:09 +0100

    Using lists for binning now and setting axis range for percent of ghosts.

--- plot_GhostRate.py
+++ plot_GhostRate.py
@@ -8,22 +8,26 @@ from CreateHistograms import *
 gROOT.Reset()
 gROOT.SetBatch(kTRUE);
 
+binningDict = {}
+binningDict["eta"] = [100, -2.6, 2.6]
+binningDict["phi"] = [100, -3.2, 3.2]
+binningDict["pt"] = [100, 0, 200]
+
 efficiencyList = []
 # Entries: Label for histogram (Will be used for filename and title) | binning | parameters used for project functions
-efficiencyList.append(["mu_recoEta", 50, -2.5, 2.5, "Eta_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"]])
+efficiencyList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
+efficiencyList.append(["mu_recoPhi", binningDict["phi"], "Phi1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
+efficiencyList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
 # TODO: Add invariant mass calculation.
 
 rateList = []
 # Entries: Label for histogram (Will be used for filename and title) | binning | parameters used for project functions
-rateList.append(["mu_recoPt", 25, 0, 50, "pT_reco", cutDict["recoPt1"]])
-rateList.append(["mu_recoPt", 25, 0, 50, "pT_reco", cutDict["gmtPt1"]])
-rateList.append(["mu_recoPt", 25, 0, 50, "pT_reco", cutDict["diMu-gmtPt1"]])
-rateList.append(["mu_recoEta", 25, -2.5, 2.5, "Eta_reco", cutDict["recoPt1"]])
-rateList.append(["mu_recoEta", 25, -2.5, 2.5, "Eta_reco", cutDict["gmtPt1"]])
-rateList.append(["mu_recoEta", 25, -2.5, 2.5, "Eta_reco", cutDict["diMu-gmtPt1"]])
-rateList.append(["mu_recoPhi", 25, -3.2, 3.2, "Phi_reco", cutDict["recoPt1"]])
-rateList.append(["mu_recoPhi", 25, -3.2, 3.2, "Phi_reco", cutDict["gmtPt1"]])
-rateList.append(["mu_recoPhi", 25, -3.2, 3.2, "Phi_reco", cutDict["diMu-gmtPt1"]])
+rateList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["recoPt1"]])
+rateList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["diMu-gmtPt1"]])
+rateList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["recoPt1"]])
+rateList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["diMu-gmtPt1"]])
+rateList.append(["mu_recoPhi", binningDict["phi"], "Phi1_reco", cutDict["recoPt1"]])
+rateList.append(["mu_recoPhi", binningDict["phi"], "Phi1_reco", cutDict["diMu-gmtPt1"]])
 
 f = TFile.Open("SingleMuNtuple.root")
 
