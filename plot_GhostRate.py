a96bc6f00fc4d23a245fed2c011f87a60b394a3d
Dinyar Rabady <dinyar.rabady@cern.ch>
2015-03-02 12:05:46 +0100

    Moved binning dict to helper function and cosmetic changes.

--- plot_GhostRate.py
+++ plot_GhostRate.py
@@ -8,11 +8,6 @@ from CreateHistograms import *
 gROOT.Reset()
 gROOT.SetBatch(kTRUE);
 
-binningDict = {}
-binningDict["eta"] = [100, -2.6, 2.6]
-binningDict["phi"] = [100, -3.2, 3.2]
-binningDict["pt"] = [100, 0, 200]
-
 efficiencyList = []
 # Entries: Label for histogram (Will be used for filename and title) | binning | parameters used for project functions
 efficiencyList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
@@ -22,19 +17,19 @@ efficiencyList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["diMu
 
 rateList = []
 # Entries: Label for histogram (Will be used for filename and title) | binning | parameters used for project functions
-rateList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["recoPt1"]])
-rateList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["diMu-gmtPt1"]])
-rateList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["recoPt1"]])
-rateList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["diMu-gmtPt1"]])
-rateList.append(["mu_recoPhi", binningDict["phi"], "Phi1_reco", cutDict["recoPt1"]])
-rateList.append(["mu_recoPhi", binningDict["phi"], "Phi1_reco", cutDict["diMu-gmtPt1"]])
+rateList.append(["mu_recoPt", binningDict["ptFine"], "pT1_reco", cutDict["recoPt1"]])
+rateList.append(["mu_recoPt", binningDict["ptFine"], "pT1_reco", cutDict["diMu-gmtPt1"]])
+rateList.append(["mu_recoEta", binningDict["etaFine"], "Eta1_reco", cutDict["recoPt1"]])
+rateList.append(["mu_recoEta", binningDict["etaFine"], "Eta1_reco", cutDict["diMu-gmtPt1"]])
+rateList.append(["mu_recoPhi", binningDict["phiFine"], "Phi1_reco", cutDict["recoPt1"]])
+rateList.append(["mu_recoPhi", binningDict["phiFine"], "Phi1_reco", cutDict["diMu-gmtPt1"]])
 
 f = TFile.Open("SingleMuNtuple.root")
 
 ntuple = f.Get("ntuple")
 
 for varList in efficiencyList:
-    generateEfficiencyHist(varList)
+    generateGhostPercHist(varList)
 
 for varList in rateList:
     generateRateHist(varList)
