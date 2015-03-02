77391f15ff01bb8735b8d1328a3a4b2911c0f704
Dinyar Rabady <dinyar@rabady.at>
2015-03-02 12:10:13 +0100

    Fixed key bug.

--- plot_GhostRate.py
+++ plot_GhostRate.py
@@ -10,9 +10,9 @@ gROOT.SetBatch(kTRUE);
 
 efficiencyList = []
 # Entries: Label for histogram (Will be used for filename and title) | binning | parameters used for project functions
-efficiencyList.append(["mu_recoEta", binningDict["eta"], "Eta1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
-efficiencyList.append(["mu_recoPhi", binningDict["phi"], "Phi1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
-efficiencyList.append(["mu_recoPt", binningDict["pt"], "pT1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
+efficiencyList.append(["mu_recoEta", binningDict["etaFine"], "Eta1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
+efficiencyList.append(["mu_recoPhi", binningDict["phiFine"], "Phi1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
+efficiencyList.append(["mu_recoPt", binningDict["ptFine"], "pT1_reco", cutDict["diMu-gmtPt1"], cutDict["recoPt1"], [0, 0.05]])
 # TODO: Add invariant mass calculation.
 
 rateList = []
